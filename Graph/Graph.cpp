9e11f120f7c9e3a3779dfc7c0848306a64554d19
Alunchik <alolachka@gmail.com>
2022-11-23 22:58:31 +0300

    graph

---
+++ Graph/Graph.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Edge { //ребро графа
+    int source; // Исходная вершина
+    int destination; // Конечная вершина
+    int weight; //вес ребра
+
+    Edge(){} // конструкторы
+    Edge(int source, int destination, int weight) {
+        this->source = source;
+        this->destination = destination;
+        this->weight = weight;
+    }
+    void print() {
+        cout << source << "<------>" << destination << "\t(" << weight << ")" << endl;
+    }
+};
+
+class Graph {
+private:
+    int verticesNum;  // количество вершин в графе
+    int edgesNum;  // количество ребер в графе
+    vector<Edge> edges; // массив ребер 
+    int** matrix; // матрица смежности
+public:
+    Graph(int vnum, int edgnum) { //конструктор, аргументы - числа вершин и ребер
+        this->verticesNum = vnum;
+        this->edgesNum = edgnum;
+        // осуществляем "заполнение" графа
+        for (int i = 0; i < edgnum; i++)
+        {
+            // x, y - номера соединенных вершин, w - вес ребра, соед. x и y
+            int x, y, w;
+            // считываем информацию о каждом ребре
+            cout << "Введите 2 вершины и вес ребра: ";
+            cin >> x >> y >> w;
+            Edge e(x, y, w); // создаем объекта классы ребра
+            edges.push_back(e); // добавляем его в наш массив
+        }
+
+        this->matrix = new int*[verticesNum]; //создаем матрицу смежности
+        for (int i = 0; i < verticesNum; i++) {
+            this->matrix[i] = new int[verticesNum];
+        }
+        for (int i = 0; i < verticesNum; i++) {
+            for (int j = 0; j < verticesNum; j++) {
+                this->matrix[i][j] = 0;
+        }
+        }
+        for (int i = 0; i < edges.size(); i++) {
+            Edge edge = edges[i];
+            matrix[edge.source][edge.destination] = edge.weight;
+            matrix[edge.destination][edge.source] = edge.weight;
+        }
+    }
+
+
+    Edge* primAlg(int start) { //алгоритм прима
+            vector not_used
+        }
+};
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ binaryTree/binaryTree.cpp
@@ -0,0 +1,180 @@
+﻿#include <iostream>
+using namespace std;
+using namespace std;
+class node
+{
+public:
+	int key;
+	node* left;
+	node* right;
+	int height;
+};
+// Функция получения высоты дерева
+int height(node* N)
+{
+	if (N == NULL)
+		return 0;
+	return N->height;
+}
+// Функция сравнения
+int max(int a, int b)
+{
+	return (a > b) ? a : b;
+}
+node* newNode(int key)
+{
+	node* nod = new node();
+	nod->key = key;
+	nod->left = NULL;
+	nod->right = NULL;
+	nod->height = 1;
+	return(nod);
+}
+// Поворот дерева вправо около корня у
+node* rightRotate(node* y)
+{
+	node* x = y->left;
+	node* T2 = x->right;
+	// Поворот
+	x->right = y;
+	y->left = T2;
+	// Новые высоты
+	y->height = max(height(y->left),
+		height(y->right)) + 1;
+	x->height = max(height(x->left),
+		height(x->right)) + 1;
+	// Возвращение нового корня
+	return x;
+}
+// Поворот дерева около корня х
+node* leftRotate(node* x)
+{
+	node* y = x->right;
+	node* T2 = y->left;
+	// Поворот
+	y->left = x;
+	x->right = T2;
+	// Новые высоты
+	x->height = max(height(x->left),
+		height(x->right)) + 1;
+	y->height = max(height(y->left),
+		height(y->right)) + 1;
+	// Возвращение нового корня
+	return y;
+}
+// вычисление баланса левого и правого дерева
+int getBalance(node* p) {
+	return (height(p->right) - height(p->left));
+}
+// Выставка элемента
+node* insert(node* pNode, int key)
+{
+	/* 1. Обычная вставка элемента в бинарном дереве */
+	if (pNode == NULL)
+		return(newNode(key));
+	if (key < pNode->key)
+		pNode->left = insert(pNode->left, key);
+	else if (key > pNode->key)
+		pNode->right = insert(pNode->right, key);
+	else // Одинаковые ключи не разрешены
+		return pNode;
+	/* 2. Обновление высоты корневого узла */
+	pNode->height = 1 + max(height(pNode->left),
+		height(pNode->right));
+	/* Вычисление баланса корневого узла чтобы понять стал ли он
+	несбалансированным */
+	int balance = getBalance(pNode);
+	// Если узел несбалансирован:
+	// Left Left Case
+	if (balance > 1) {
+		if (key < pNode->right->key)
+		{
+			pNode->right = rightRotate(pNode->right);
+			return leftRotate(pNode);
+		}
+		else if (key > pNode->right->key)
+			return leftRotate(pNode);
+	}
+	else if (balance < -1)
+	{
+		if (key > pNode->left->key)
+		{
+			pNode->left = leftRotate(pNode->left);
+			return rightRotate(pNode);
+		}
+		else if (key < pNode->left->key)
+			return rightRotate(pNode);
+	}
+	/* Если узел сбалансирован, возвращаем не меняя */
+	return pNode;
+}
+
+/* Симметричный обход */
+void inOrder(node* root) {
+	if (root != NULL)
+	{
+		inOrder(root->left);
+		cout << root->key << " ";
+		inOrder(root->right);
+	}
+}
+void summa(node* node, int& sum)
+{
+	if (!node) { return; }
+	summa(node->left, sum);
+	summa(node->right, sum);
+	sum += node->key;
+}
+void count(node* node, int& num)
+{
+	if (!node) { return; }
+	count(node->left, num);
+	count(node->right, num);
+	(num)++;
+}
+
+//среднее арифметическое узлов дерева
+double average(node* node)
+{
+	int s = 0, c = 0;
+	summa(node, s);
+	count(node, c);
+	return ((double)s) / ((double)c);
+}
+int main() {
+	node* root = NULL;
+	int sum = 0, command = -1, input = 0;
+	while (command != 0) {
+		cout << "Выберите команду";
+		cout << "1 - ввод элементов дерева\n";
+		cout << "2 - симметричный обход\n";
+		cout << "3 - среднее арифметическое\n";
+		cout << "4 - высота дерева\n";
+		cout << "0 - выход\n";
+		cin >> command;
+		switch (command) {
+		case 1:
+			cout << "\n введите 10 значений" << endl;
+			cout << "\n Input 10 keys: " << endl;
+			for (int i = 0; i < 10; i++) {
+				cin >> input;
+				root = insert(root, input);
+			}
+			cout << endl;
+			break;
+		case 2:
+			cout << "Симметричный обход " << endl;
+			inOrder(root);
+			cout << endl;
+			break;
+		case 3:
+			cout << average(root) << endl;
+			break;
+		case 4:
+			cout << height(root);
+		case 5:
+			return 0;
+		}
+	}
+	return 0;
+}
